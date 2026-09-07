e4729397e5acdb7f7955a98be3bb8313159ce039
Michelbvp <michelbvp@gmail.com>
2026-09-07 23:41:32 +0000

    Adiciona exemplos de variáveis e tipos

--- licao2.py
+++ licao2.py
@@ -1,17 +1,9 @@
 #dados de uma transação.
-descricao = "Pagamento fornecedor XPTO"
-valor = 15000.00
-limite_alcada = 10000.00
+descricao = "Entrada de nota fiscal"
+centro_de_custo = "Compras"
+valor = 10000.00
+limite_alcada = 5000.00
 aprovada = True
 print(descricao)
-print(valor)
-print(valor > limite_alcada)
-
-descricao1 = "Entrada de nota fiscal"
-centro_de_custo = "Compras"
-valor1 = 10000.00
-limite_alcada1 = 5000.00
-aprovada1 = True
-print(descricao1)
-print(centro_de_custo)
-print(type(aprovada1))
+print(valor == limite_alcada)
+print(type(aprovada))
